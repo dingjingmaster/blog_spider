5348d50e913b036a3f59ebc9068e9da2cb802bfb
dingjingmaster <dingjing@live.cn>
2019-11-04 23:26:07 +0800

    可能存在的崩溃问题

--- frame/common/util.py
+++ frame/common/util.py
@@ -7,9 +7,9 @@ from urllib.parse import unquote
 class Util:
     @staticmethod
     def check_url (url: str, base_url: str) -> str:
-        if not url.startswith("https://") or url.startswith("http://"):
-            url = base_url + '/' + url
         try:
+            if not url.startswith("https://") or url.startswith("http://"):
+                url = base_url + '/' + url
             url = unquote(url, 'utf8')
         except Exception:
             url = ''
